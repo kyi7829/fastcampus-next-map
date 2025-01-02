7c9a6269cc1a352e0afbb9a69209acc3d9b494ca
kyi7829 <yi7829@naver.com>
2025-01-02 22:37:15 +0900

    dataFetching SSR/SSG 실습 (getServerSideProps, getStaticProps, ISR)

---
+++ src/pages/dataFetching/getServerSideProps.tsx
@@ -0,0 +1,22 @@
+// https://nextjs.org/docs/pages/api-reference/functions/get-server-side-props
+
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
+export const getServerSideProps = (async () => {
+    // Fetch data from external API
+    const num = await fetch('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new')
+    const number = await num.json()
+    // Pass data to the page via props
+    return { props: { number } }
+}) satisfies GetServerSideProps<{ number: number }>
+
+export default function Page({
+                                 number,
+                             }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    return (
+        <div>
+            <h1>getServerSideProps</h1>
+            <h2>number: {number}</h2>
+        </div>
+    )
+}
