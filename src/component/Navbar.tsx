f042b7e1f298e0afe3052386edfceb77eb76fe7c
YeongInKim <yi7829@naver.com>
2025-02-03 16:02:43 +0900

    tailwindcss 적용

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import Link from "next/link";
 
+import { MdMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <>
             <div className="navbar">
-                <div className="navbar__logo">nextmap</div>
+                <Link className="navbar__logo" href="/">nextmap</Link>
                 <div className="navbar__list">
                     <Link href="/stores" className="navbar__list--item">
                         맛집 목록
@@ -21,7 +24,33 @@ export default function Navbar() {
                         로그인
                     </Link>
                 </div>
+                {/* mobile button */}
+                <div
+                    role="presentation"
+                    className="navbar__button"
+                    onClick={() => setIsOpen((val) => !val)}>
+                    {isOpen ? <MdClose /> : <MdMenu />}
+                </div>
             </div>
+            {/* mobile navbar */}
+            {isOpen && (
+                <div className="navbar--mobile">
+                    <div className="navbar__list--mobile">
+                        <Link href="/stores" className="navbar__list--item--mobile">
+                            맛집 목록
+                        </Link>
+                        <Link href="/stores/new" className="navbar__list--item--mobile">
+                            맛집 등록
+                        </Link>
+                        <Link href="/users/likes" className="navbar__list--item--mobile">
+                            찜한 가게
+                        </Link>
+                        <Link href="/users/login" className="navbar__list--item--mobile">
+                            로그인
+                        </Link>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
