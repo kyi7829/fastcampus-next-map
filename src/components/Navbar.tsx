088039fedde1d5554c68403ceae739bb2604f705
kyi7829 <yi7829@naver.com>
2025-03-01 16:13:51 +0900

    구글로그인 적용

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-import Link from "next/link";
 import { useState } from "react";
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
 
 
 export default function Navbar(){
     const [isOpen, setIsOpen] = useState(false);
+    const { data, status } = useSession();
     return (
         <>
             <div className="navbar">
@@ -14,7 +17,15 @@ export default function Navbar(){
                     <Link href="/stores" className="navbar__list--item">맛집 목록</Link>
                     <Link href="/stores/new" className="navbar__list--item">맛집 등록</Link>
                     <Link href="/users/likes" className="navbar__list--item">찜한 가게</Link>
-                    <Link href="/api/auth/signin" className="navbar__list--item">로그인</Link>
+                    {status === "authenticated" ? (
+                        <button type="button" onClick={() => signOut()}>
+                            로그아웃
+                        </button>
+                    ) : (
+                        <Link href="/api/auth/signin" className="navbar__list--item">
+                            로그인
+                        </Link>
+                    )}
                 </div>
                 {/* mobile button */}
                 <div role="presentation" className="navbar__button" onClick={()=> setIsOpen((val) => !val)}>
