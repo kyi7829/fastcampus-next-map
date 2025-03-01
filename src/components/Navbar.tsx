4987f1c287715728ca6469f3cd3b7f678e4254f1
kyi7829 <yi7829@naver.com>
2025-03-01 15:52:01 +0900

    Nextauth 적용, middleware 적용

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar(){
                     <Link href="/stores" className="navbar__list--item">맛집 목록</Link>
                     <Link href="/stores/new" className="navbar__list--item">맛집 등록</Link>
                     <Link href="/users/likes" className="navbar__list--item">찜한 가게</Link>
-                    <Link href="/users/login" className="navbar__list--item">로그인</Link>
+                    <Link href="/api/auth/signin" className="navbar__list--item">로그인</Link>
                 </div>
                 {/* mobile button */}
                 <div role="presentation" className="navbar__button" onClick={()=> setIsOpen((val) => !val)}>
@@ -28,7 +28,7 @@ export default function Navbar(){
                     <Link href="/stores" className="navbar__list--item--mobile">맛집 목록</Link>
                     <Link href="/stores/new" className="navbar__list--item--mobile">맛집 등록</Link>
                     <Link href="/users/likes" className="navbar__list--item--mobile">찜한 가게</Link>
-                    <Link href="/users/login" className="navbar__list--item--mobile">로그인</Link>
+                    <Link href="/api/auth/signin" className="navbar__list--item--mobile">로그인</Link>
                     </div>
                 </div>
             )}
