a4dbb2310eaf18a8b99475bcfd74df32f2a0f550
kyi7829 <yi7829@naver.com>
2025-03-03 13:16:37 +0900

    맛집 목록 검색 컴포넌트

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter} from "@auth/prisma-adapter";
-import {PrismaClie, PrismaClient} from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
 import GoogleProvider from "next-auth/providers/google";
 import NaverProvider from "next-auth/providers/naver";
 import KakaoProvider from "next-auth/providers/kakao";
@@ -8,6 +8,11 @@ import KakaoProvider from "next-auth/providers/kakao";
 const prisma = new PrismaClient();
 
 export const authOptions = {
+    session: {
+        strategy: "jwt" as const,
+        maxAge: 60 * 60 * 24,
+        updateAge: 60 * 60 * 2,
+    },
     adapter: PrismaAdapter(prisma),
     // Configure one or more authentication providers
     providers: [
