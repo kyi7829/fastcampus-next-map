60fa6cab02cb69e153a8f1cb1012bba687be1deb
kyi7829 <yi7829@naver.com>
2025-03-03 13:31:21 +0900

    맛집 목록 검색 완료

--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useRef, useEffect, useCallback, useState } from "react";
 
 import Image from "next/image";
 import { StoreType } from "@/interface";
@@ -19,12 +19,20 @@ export default function StoreListPage() {
     const ref = useRef<HTMLDivElement | null>(null);
     const pageRef = useIntersectionObserver(ref, {});
     const isPageEnd = !!pageRef?.isIntersecting;
+    const [q, setQ] = useState<string | null>(null);
+    const [district, setDistrict] = useState<string | null>(null);
+
+    const searchParams = {
+        q: q,
+        district: district,
+    };
 
     const fetchStores = async ({ pageParam = 1 }) => {
         const { data } = await axios("/api/stores?page=" + pageParam, {
             params: {
                 limit: 10,
                 page: pageParam,
+                ...searchParams,
             },
         });
 
@@ -39,7 +47,7 @@ export default function StoreListPage() {
         hasNextPage,
         isError,
         isLoading,
-    } = useInfiniteQuery("stores", fetchStores, {
+    } = useInfiniteQuery(["stores", searchParams], fetchStores, {
         getNextPageParam: (lastPage: any) =>
             lastPage.data?.length > 0 ? lastPage.page + 1 : undefined,
     });
@@ -74,7 +82,7 @@ export default function StoreListPage() {
     return (
         <div className="px-4 md:max-w-4xl mx-auto py-8">
             {/* search filter */}
-            <SearchFilter />
+            <SearchFilter setQ={setQ} setDistrict={setDistrict} />
             <ul role="list" className="divide-y divide-gray-100">
                 {isLoading ? (
                     <Loading />
